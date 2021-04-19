0b14873fdd32c620c151c5f01125def3bc4390ae
Ankush <Ankush@DESKTOP-G7HDG2O>
2021-04-19 17:09:09 +0530

    By web core stone

--- GitDemo/src/TestDemo.java
+++ GitDemo/src/TestDemo.java
@@ -2,6 +2,10 @@
 public class TestDemo {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git");
+		
+	}
+	public void sum() {
+		System.out.println("Sum method is added");
 	}
 
 }
